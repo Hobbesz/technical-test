e36a773db63e7dd7d317980217cf9ef337ad9186
calvin-james <calvin.james@hotmail.co.uk>
2024-11-30 22:05:28 +0000

    feat: setup laravel ui with vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-require('./bootstrap');
+import './bootstrap';
+import { createApp } from 'vue';
+import HomeView from './Views/HomeView.vue';
+
+const app = createApp({});
+
+const files = require.context('./Views', true, /\.vue$/i)
+files.keys().map(key => app.component(key.split('/').pop().split('.')[0], files(key).default))
+
+app.mount('#app');
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\NoteController;
 use App\Http\Controllers\WindFarmController;
 use Illuminate\Http\Request;
@@ -15,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('/authenticate', [AuthController::class, 'authenticate']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
